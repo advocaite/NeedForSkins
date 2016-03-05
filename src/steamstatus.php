2e27ada5219c069bed4bb1ed6aa7dcedf8bd70ae
mvegter197 <m.vegter2000@gmail.com>
2016-03-05 08:56:18 +0100

    Removed Unused Lines

--- src/steamstatus.php
+++ src/steamstatus.php
@@ -15,8 +15,6 @@ $response=json_decode($get);
 if($response->success=='true'){
 	echo'<table style="float:right;color:gray;">';
 	if(!empty($response->psa) && preg_match('/down|offline/i', $response->psa) && !preg_match('/bot/i', $response->psa)){
-		//echo'<tr><td colspan="2">'.$response->psa.'</td></tr>';
-		//echo'<tr><td colspan="2"><b>Steam is down.</b></td></tr>';
 	}
 
 	echo'<tr><td>inventories:</td><td align="right" style="padding-left:15px;'.(($response->services->csgo_community->status=='major')?'color:red':'').'">'.(($response->services->csgo_community->title=='Unknown') ? 'API Offline' : $response->services->csgo_community->title).'</td></tr>';
@@ -25,12 +23,3 @@ if($response->success=='true'){
 	echo'<tr><td colspan="2" align="right"><small style="color:lightgray;">stats by <a href="http://steamstat.us/" target="_blank" style="color:lightgray !important;text-decoration:underline;">steamstat.us</a></small></td></tr>';
 	echo'</table>';
 }
-//var_dump($response);
-
-/*if($response->services->store->status=='major' || $response->services->community->status=='major' || $response->services->csgo_community->status=='major'){
-	echo'<center><b>WARNING!</b><br/>Steam servers are experiencing issues at the moment! Your offers may be delayed or not go through at all!<br/><br/>';
-	echo'Steam store: '.$response->services->store->title.'<br/>';
-	echo'Steam community: '.$response->services->community->title.'<br/>';
-	echo'CS:GO inventories: '.$response->services->csgo_community->title.'<br/>';
-	echo'</center>';
-}else{*/
